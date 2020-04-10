fd6c6628f31eca970bbfa5dc0387d72f6999cdd6
PaulSpuzello <46540297+PaulSpuzello@users.noreply.github.com>
2020-04-10 11:51:46 -0500

    FInished selectEvents

--- selectEvents.php
+++ selectEvents.php
@@ -4,26 +4,27 @@ session_start();
 		
 		require 'dbConnect.php';
 		
-		$sql = "SELECT ";
-		$sql .= "event_id, ";
+        $sql = "SELECT ";
+		$sql .= "event_id, ";       
 		$sql .= "event_name, ";
 		$sql .= "event_description, ";
 		$sql .= "event_presenter, ";
-		$sql .= "event_date, ";
-		$sql .= "event_time ";
+        $sql .= "event_date, ";
+		$sql .= "event_time ";       
 		
-		$sql .= "FROM wdv341_event";
-		$sql .= "WHERE event_id = 2";
-		
+		$sql .= "FROM wdv341_event ";
+        
 		$stmt = $conn->prepare($sql);
 		$stmt->execute();
 		
 		$stmt->setFetchMode(PDO::FETCH_ASSOC);
-		
+    
+
 	}
 	
 	catch(PDOException $e) {
-		$message = "There has been a problem, please try again later.";
+        $message = "There has been a problem, please try again later.";
+        echo $message;
 	}
 	
 ?>
@@ -31,41 +32,47 @@ session_start();
 <!doctype html>
 <html>
 <head>
+<style>
+	table {
+		border-collapse: collapse;
+	}
+	
+	td, th {
+		border: 1px solid #808080;
+		text-align: left;
+		padding: 8px;
+	}
+</style>
 	<meta charset="utf-8">
 </head>
 <body>
-<div id="container">
-	<header>
-		<h1>Events</h1>
-	</header>
-	<main>
-		<?php
-			while( $row=$stmt->fetch(PDO::FETCH_ASSOC)) {
-		?>
-			<div class="eventBlock">
-				<div class="row">
-					<span class="eventId"><?php echo $row['event_id']; ?></span>
-				</div>
-				<div class="row">
-					<span class="eventName"><?php echo $row['event_name']; ?></span>
-				</div>     
-				<div class="row">
-					<span class="eventDesc"><?php echo $row['event_description']; ?></span>
-				</div>
-				<div class="row">
-					<span class="eventPresenter"><?php echo $row['event_presenter']; ?></span>
-				</div>
-				<div class="row">
-					<span class="eventDate"><?php echo $row['event_date']; ?></span>
-				</div>
-				<div class="row">
-					<span class="eventTime"><?php echo $row['event_time']; ?></span>
-				</div>
-			</div>
+<header>
+	<h1>Events</h1>
+</header>
+<table>
+	<tr>
+		<th>Event ID</th>
+		<th>Event Name</th>
+		<th>Event Desc.</th>
+		<th>Event Presenter</th>
+		<th>Event Date</th>
+		<th>Event Time</th>
+	</tr>
+	<?php
+		while( $row=$stmt->fetch(PDO::FETCH_ASSOC)) {
+	?>
+	<tr>
+		<th><?php echo $row['event_id']; ?></th>
+		<th><?php echo $row['event_name']; ?></th>
+		<th><?php echo $row['event_description']; ?></th>
+		<th><?php echo $row['event_presenter']; ?></th>
+		<th><?php echo $row['event_date']; ?></th>
+		<th><?php echo $row['event_time']; ?></th>
+	</tr>
 	<?php
 		}
 	?>
-	</main>
+
 </div>
 </body>
 </html>
